3700aa6ddfe77fca3941968c9afa6a4cd9e69932
udy2004 <udemeedetumana@gmail.com>
2021-12-07 12:03:43 +0000

    Added a comment to descibe my code.

--- password .py
+++ password .py
@@ -1,11 +1,11 @@
-"""Creating a container that adds your username and password and allows you access to something"""
+
 def username():
+    """Creating a container that adds your username and password and allows you access to something"""
     names = []
     password = []
     name = input("Enter your username or email: ")
     passwords = input("Enter your password here: ")
-    """using the append to add both your username and password to names/password"""
-    names.append(name)
+    names.append(name) # using the append to add both your username and password to names/password
     password.append(passwords)
     
     print("\nplease login:")
@@ -13,6 +13,7 @@ def username():
     secret_key = input("Enter your password: ")
     
     if login == name and secret_key == passwords:
+        """using the if statement"""
         print(f"Welcome Back {login}")
     else:
         print("who you?")
