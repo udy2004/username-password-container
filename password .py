f77ecd680fd3cc7eb738bf9d8c6eb92b97abe379
udy2004 <udemeedetumana@gmail.com>
2021-12-07 11:57:57 +0000

    updated repo

--- password .py
+++ password .py
@@ -1,8 +1,10 @@
+"""Creating a container that adds your username and password and allows you access to something"""
 def username():
     names = []
     password = []
     name = input("Enter your username or email: ")
     passwords = input("Enter your password here: ")
+    """using the append to add both your username and password to names/password"""
     names.append(name)
     password.append(passwords)
     
